a2226a9d77b9a3b16399b00b45f4575956738f54
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-11-01 20:42:08 +0600

    Updated Widget

--- elements.php
+++ elements.php
@@ -66,6 +66,31 @@ if (file_exists(EEA_PLUGIN_PATH . "autoload.php")) {
     require_once  EEA_PLUGIN_PATH . "autoload.php";
 }
 
+// Enqueue
+// add_action('eael/before_enqueue_styles', 'before_enqueue_styles');
+add_action('elementor/editor/before_enqueue_scripts', 'editor_enqueue_scripts');
+// add_action('elementor/frontend/before_register_scripts', 'frontend_enqueue_scripts');
+// editor styles
+function editor_enqueue_scripts()
+{
+    // ea icon font
+    wp_enqueue_style(
+        'box-icons',
+        // EEA_PLUGIN_URL . 'assets/admin/css/eaicon.css',
+        "https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css",
+        false,
+        EEA_PLUGIN_VERSION
+    );
+
+    // editor style
+    wp_enqueue_style(
+        'eea-editor',
+        EEA_PLUGIN_URL . 'assets/admin/css/editor.css',
+        false,
+        EEA_PLUGIN_VERSION
+    );
+}
+
 /**
  * Register New Widget.
  *
@@ -81,5 +106,38 @@ if (!function_exists("register_eea_widget")) {
     {
         require_once(EEA_PLUGIN_PATH . 'includes/Elements/Widget.php');
         $widget?->register(new \AwesomeCoder\Elements\Elementor_Elements_Widget());
+    }
+}
+
+/**
+ * Register New Widget.
+ *
+ * Include widget file and register widget class.
+ *
+ * @since 1.0.0
+ * @param \Elementor\Widgets_Manager $widget Elementor widgets manager.
+ * @return void
+ */
+add_action('elementor/elements/categories_registered', 'elementor_add_eea_widget_categories');
+if (!function_exists("elementor_add_eea_widget_categories")) {
+    function elementor_add_eea_widget_categories($elements_manager)
+    {
+
+        // $elementor->add_category(
+        //     'eea',
+        //     [
+        //         'title' => esc_html__('First Category', 'elements'),
+        //         'icon' => 'fa fa-plug',
+        //     ]
+        // );
+
+        $elements_manager->add_category(
+            'elements-addons-elementor',
+            [
+                'title' => __('Elements Addons', EEA_PLUGIN_TEXTDOMAIN),
+                'icon'  => 'font',
+            ],
+            1
+        );
     }
 }
--- includes/Elements/Widget.php
+++ includes/Elements/Widget.php
@@ -17,45 +17,49 @@ namespace AwesomeCoder\Elements;
  *
  */
 
-class Elementor_Elements_Widget extends \Elementor\Widget_Base {
+class Elementor_Elements_Widget extends \Elementor\Widget_Base
+{
 
 	/**
 	 * Get widget name.
 	 *
-	 * Retrieve list widget name.
+	 * Retrieve oEmbed widget name.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 * @return string Widget name.
 	 */
-	public function get_name() {
-		return 'eee_list';
+	public function get_name()
+	{
+		return 'eea-embed';
 	}
 
 	/**
 	 * Get widget title.
 	 *
-	 * Retrieve list widget title.
+	 * Retrieve oEmbed widget title.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 * @return string Widget title.
 	 */
-	public function get_title() {
-		return esc_html__( 'EEE List', EEA_PLUGIN_TEXTDOMAIN );
+	public function get_title()
+	{
+		return esc_html__('EEA Embed', 'elementor-oembed-widget');
 	}
 
 	/**
 	 * Get widget icon.
 	 *
-	 * Retrieve list widget icon.
+	 * Retrieve oEmbed widget icon.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 * @return string Widget icon.
 	 */
-	public function get_icon() {
-		return 'eicon-bullet-list';
+	public function get_icon()
+	{
+		return 'eea-icon bx bx-link';
 	}
 
 	/**
@@ -67,339 +71,199 @@ class Elementor_Elements_Widget extends \Elementor\Widget_Base {
 	 * @access public
 	 * @return string Widget help URL.
 	 */
-	public function get_custom_help_url() {
-		return 'https://developers.elementor.com/docs/widgets/';
+	public function get_custom_help_url()
+	{
+		return 'https://awesomecoder.dev/';
 	}
 
 	/**
 	 * Get widget categories.
 	 *
-	 * Retrieve the list of categories the list widget belongs to.
+	 * Retrieve the list of categories the oEmbed widget belongs to.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 * @return array Widget categories.
 	 */
-	public function get_categories() {
-		return [ 'general' ];
+	public function get_categories()
+	{
+		return ['elements-addons-elementor'];
 	}
 
 	/**
 	 * Get widget keywords.
 	 *
-	 * Retrieve the list of keywords the list widget belongs to.
+	 * Retrieve the list of keywords the oEmbed widget belongs to.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 * @return array Widget keywords.
 	 */
-	public function get_keywords() {
-		return [ 'eee','eee list','list', 'lists', 'ordered', 'unordered' ];
+	public function get_keywords()
+	{
+		return ['eea', 'oembed', 'url', 'link'];
 	}
 
 	/**
-	 * Register list widget controls.
+	 * Register oEmbed widget controls.
 	 *
 	 * Add input fields to allow the user to customize the widget settings.
 	 *
 	 * @since 1.0.0
 	 * @access protected
 	 */
-	protected function register_controls() {
+	protected function register_controls()
+	{
 
 		$this->start_controls_section(
 			'content_section',
 			[
-				'label' => esc_html__( 'List Content', EEA_PLUGIN_TEXTDOMAIN ),
+				'label' => esc_html__('Content', EEA_PLUGIN_TEXTDOMAIN),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
 
-		/* Start repeater */
-
-		$repeater = new \Elementor\Repeater();
-
-		$repeater->add_control(
-			'text',
+		$this->add_control(
+			'url',
 			[
-				'label' => esc_html__( 'Text', EEA_PLUGIN_TEXTDOMAIN ),
+				'label' => esc_html__('URL to embed', EEA_PLUGIN_TEXTDOMAIN),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'placeholder' => esc_html__( 'List Item', EEA_PLUGIN_TEXTDOMAIN ),
-				'default' => esc_html__( 'List Item', EEA_PLUGIN_TEXTDOMAIN ),
-				'label_block' => true,
-				'dynamic' => [
-					'active' => true,
-				],
+				'input_type' => 'url',
+				'placeholder' => esc_html__('https://your-link.com', EEA_PLUGIN_TEXTDOMAIN),
 			]
 		);
 
-		$repeater->add_control(
-			'link',
-			[
-				'label' => esc_html__( 'Link', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::URL,
-				'placeholder' => esc_html__( 'https://your-link.com', EEA_PLUGIN_TEXTDOMAIN ),
-				'dynamic' => [
-					'active' => true,
-				],
-			]
-		);
-
-		/* End repeater */
 
 		$this->add_control(
-			'list_items',
+			'title',
 			[
-				'label' => esc_html__( 'List Items', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $repeater->get_controls(),           /* Use our repeater */
-				'default' => [
-					[
-						'text' => esc_html__( 'List Item #1', EEA_PLUGIN_TEXTDOMAIN ),
-						'link' => '',
-					],
-					[
-						'text' => esc_html__( 'List Item #2', EEA_PLUGIN_TEXTDOMAIN ),
-						'link' => '',
-					],
-					[
-						'text' => esc_html__( 'List Item #3', EEA_PLUGIN_TEXTDOMAIN ),
-						'link' => '',
-					],
-				],
-				'title_field' => '{{{ text }}}',
-			]
-		);
-
-		$this->end_controls_section();
-
-		$this->start_controls_section(
-			'marker_section',
-			[
-				'label' => esc_html__( 'List Marker', EEA_PLUGIN_TEXTDOMAIN ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'label' => esc_html__('Title', EEA_PLUGIN_TEXTDOMAIN),
+				'placeholder' => esc_html__('Enter your title', EEA_PLUGIN_TEXTDOMAIN),
 			]
 		);
 
 		$this->add_control(
-			'marker_type',
+			'size',
 			[
-				'label' => esc_html__( 'Marker Type', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'type' => \Elementor\Controls_Manager::NUMBER,
+				'label' => esc_html__('Size', EEA_PLUGIN_TEXTDOMAIN),
+				'placeholder' => '0',
+				'min' => 0,
+				'max' => 100,
+				'step' => 1,
+				'default' => 50,
+			]
+		);
+
+		$this->add_control(
+			'open_lightbox',
+			[
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'label' => esc_html__('Lightbox', EEA_PLUGIN_TEXTDOMAIN),
 				'options' => [
-					'ordered' => [
-						'title' => esc_html__( 'Ordered List', EEA_PLUGIN_TEXTDOMAIN ),
-						'icon' => 'eicon-editor-list-ol',
-					],
-					'unordered' => [
-						'title' => esc_html__( 'Unordered List', EEA_PLUGIN_TEXTDOMAIN ),
-						'icon' => 'eicon-editor-list-ul',
-					],
-					'other' => [
-						'title' => esc_html__( 'Custom List', EEA_PLUGIN_TEXTDOMAIN ),
-						'icon' => 'eicon-edit',
-					],
+					'default' => esc_html__('Default', EEA_PLUGIN_TEXTDOMAIN),
+					'yes' => esc_html__('Yes', EEA_PLUGIN_TEXTDOMAIN),
+					'no' => esc_html__('No', EEA_PLUGIN_TEXTDOMAIN),
 				],
-				'default' => 'ordered',
-				'toggle' => false,
+				'default' => 'no',
 			]
 		);
 
 		$this->add_control(
-			'marker_content',
+			'alignment',
 			[
-				'label' => esc_html__( 'Custom Marker', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'placeholder' => esc_html__( 'Enter custom marker', EEA_PLUGIN_TEXTDOMAIN ),
-				'default' => '🧡',
-				'condition' => [
-					'marker_type[value]' => 'other',
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'label' => esc_html__('Alignment', EEA_PLUGIN_TEXTDOMAIN),
+				'options' => [
+					'left' => [
+						'title' => esc_html__('Left', EEA_PLUGIN_TEXTDOMAIN),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__('Center', EEA_PLUGIN_TEXTDOMAIN),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__('Right', EEA_PLUGIN_TEXTDOMAIN),
+						'icon' => 'eicon-text-align-right',
+					],
 				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-list-widget-text::marker' => 'content: "{{VALUE}}";',
-				],
+				'default' => 'center',
 			]
 		);
 
 		$this->end_controls_section();
 
 		$this->start_controls_section(
-			'style_content_section',
+			'section_style',
 			[
-				'label' => esc_html__( 'List Style', EEA_PLUGIN_TEXTDOMAIN ),
+				'label' => esc_html__('Style', 'textdomain'),
 				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
 			]
 		);
 
 		$this->add_control(
-			'title_color',
+			'color',
 			[
-				'label' => esc_html__( 'Color', EEA_PLUGIN_TEXTDOMAIN ),
+				'label' => esc_html__('Color', 'textdomain'),
 				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#f00',
 				'selectors' => [
-					'{{WRAPPER}} .elementor-list-widget-text' => 'color: {{VALUE}};',
-					'{{WRAPPER}} .elementor-list-widget-text > a' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			\Elementor\Group_Control_Typography::get_type(),
-			[
-				'name' => 'icon_typography',
-				'selector' => '{{WRAPPER}} .elementor-list-widget-text, {{WRAPPER}} .elementor-list-widget-text > a',
-			]
-		);
-
-		$this->add_group_control(
-			\Elementor\Group_Control_Text_Shadow::get_type(),
-			[
-				'name' => 'text_shadow',
-				'selector' => '{{WRAPPER}} .elementor-list-widget-text',
-			]
-		);
-
-		$this->end_controls_section();
-
-		$this->start_controls_section(
-			'style_marker_section',
-			[
-				'label' => esc_html__( 'Marker Style', EEA_PLUGIN_TEXTDOMAIN ),
-				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		$this->add_control(
-			'marker_color',
-			[
-				'label' => esc_html__( 'Color', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .elementor-list-widget-text::marker' => 'color: {{VALUE}};',
+					'{{WRAPPER}} h3' => 'color: {{VALUE}}',
 				],
 			]
 		);
 
 		$this->add_control(
-			'marker_spacing',
+			'image',
 			[
-				'label' => esc_html__( 'Spacing', EEA_PLUGIN_TEXTDOMAIN ),
-				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ 'px', 'em', 'rem', 'custom' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 100,
-					],
-					'em' => [
-						'min' => 0,
-						'max' => 10,
-					],
-					'rem' => [
-						'min' => 0,
-						'max' => 10,
-					],
-				],
+				'label' => __('<i class="eea bx bx-link" ></i> Choose Image', 'textdomain'),
+				'type' => \Elementor\Controls_Manager::MEDIA,
 				'default' => [
-					'unit' => 'px',
-					'size' => 40,
-				],
-				'selectors' => [
-					// '{{WRAPPER}} .elementor-list-widget' => 'padding-left: {{SIZE}}{{UNIT}};',
-					'{{WRAPPER}} .elementor-list-widget' => 'padding-inline-start: {{SIZE}}{{UNIT}};',
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
 				],
 			]
 		);
-
 		$this->end_controls_section();
-
 	}
 
 	/**
-	 * Render list widget output on the frontend.
+	 * Render oEmbed widget output on the frontend.
 	 *
 	 * Written in PHP and used to generate the final HTML.
 	 *
 	 * @since 1.0.0
 	 * @access protected
 	 */
-	protected function render() {
+	protected function render()
+	{
 		$settings = $this->get_settings_for_display();
-		$html_tag = [
-			'ordered' => 'ol',
-			'unordered' => 'ul',
-			'other' => 'ul',
-		];
-		$this->add_render_attribute( 'list', 'class', EEA_PLUGIN_TEXTDOMAIN );
-		?>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse a ipsa, amet suscipit, dolorem consequuntur adipisci nobis deserunt aspernatur magnam omnis quo qui minima aperiam impedit ab tenetur reiciendis!</p>
-		<<?php echo $html_tag[ $settings['marker_type'] ]; ?> <?php $this->print_render_attribute_string( 'list' ); ?>>
-			<?php
-			foreach ( $settings['list_items'] as $index => $item ) {
-				$repeater_setting_key = $this->get_repeater_setting_key( 'text', 'list_items', $index );
-				$this->add_render_attribute( $repeater_setting_key, 'class', 'elementor-list-widget-text' );
-				$this->add_inline_editing_attributes( $repeater_setting_key );
-				?>
-				<li <?php $this->print_render_attribute_string( $repeater_setting_key ); ?>>
-					<?php
-					$title = $settings['list_items'][$index]['text'];
 
-					if ( ! empty( $item['link']['url'] ) ) {
-						$this->add_link_attributes( "link_{$index}", $item['link'] );
-						$linked_title = sprintf( '<a %1$s>%2$s</a>', $this->get_render_attribute_string( "link_{$index}" ), $title );
-						echo $linked_title;
-					} else {
-						echo $title;
-					}
-					?>
-				</li>
-				<?php
-			}
-			?>
-		</<?php echo $html_tag[ $settings['marker_type'] ]; ?>>
-		<?php
+		echo '<div class="oembed-elementor-widget">';
+		echo '<h3>' . $settings['title'] . '</h3>';
+
+		// Get image url
+		echo '<img src="' . esc_url($settings['image']['url']) . '" alt="">';
+
+		// Get image by id
+		echo wp_get_attachment_image($settings['image']['id'], 'thumbnail');
+		echo json_encode($settings, JSON_PRETTY_PRINT);
+		echo '</div>';
 	}
-
 	/**
-	 * Render list widget output in the editor.
+	 * Render oEmbed widget output on the frontend.
 	 *
-	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 * Written in PHP and used to generate the final HTML.
 	 *
 	 * @since 1.0.0
 	 * @access protected
 	 */
-	protected function content_template() {
-		?>
-		<#
-		html_tag = {
-			'ordered': 'ol',
-			'unordered': 'ul',
-			'other': 'ul',
-		};
-		view.addRenderAttribute( 'list', 'class', EEA_PLUGIN_TEXTDOMAIN );
-		#>
-		<{{{ html_tag[ settings.marker_type ] }}} {{{ view.getRenderAttributeString( 'list' ) }}}>
-			<# _.each( settings.list_items, function( item, index ) {
-				var repeater_setting_key = view.getRepeaterSettingKey( 'text', 'list_items', index );
-				view.addRenderAttribute( repeater_setting_key, 'class', 'elementor-list-widget-text' );
-				view.addInlineEditingAttributes( repeater_setting_key );
-				#>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse a ipsa, amet suscipit, dolorem consequuntur adipisci nobis deserunt aspernatur magnam omnis quo qui minima aperiam impedit ab tenetur reiciendis!</li>
-
-				<li {{{ view.getRenderAttributeString( repeater_setting_key ) }}}>
-					<# var title = item.text; #>
-					<# if ( item.link ) { #>
-						<# view.addRenderAttribute( `link_${index}`, item.link ); #>
-						<a href="{{ item.link.url }}" {{{ view.getRenderAttributeString( `link_${index}` ) }}}>
-							{{{title}}}
-						</a>
-					<# } else { #>
-						{{{title}}}
-					<# } #>
-				</li>
-			<# } ); #>
-		</{{{ html_tag[ settings.marker_type ] }}}>
-		<?php
+	protected function content_template()
+	{
+?>
+		<h3>{{{ settings.title }}}</h3>
+		<img src="{{{ settings.image.url }}}">
+<?php
 	}
-
 }
